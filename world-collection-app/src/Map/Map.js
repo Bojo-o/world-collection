b08a5c3e8f8db4b11387df36ec92421585b969ce
Martin Bojacko <bojacko.martin@gmail.com>
2022-10-29 17:17:25 +0200

    created basic react app

---
+++ world-collection-app/src/Map/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Map.css';
+
+class Map extends React.Component{
+    render(){
+        return(
+            <h1>Here will be interatcive map</h1>
+        );
+    }
+}
+
+export default Map;
+
